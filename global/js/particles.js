fcb72020aef0277bf7bd0ccdbe663af05a56ce61
manishbjp <www.manishbajpai055@gmail.com>
2019-02-07 16:44:31 +0530

    Remove unwanted code

--- global/js/particles.js
+++ global/js/particles.js
@@ -1540,12 +1540,3 @@ window.particlesJS.load = function(tag_id, path_config_json, callback){
 
 };
 
-var n = localStorage.getItem('on_load_counter');
-
-        if (n === null) {
-            n = 0;}
-        n++;
-        localStorage.setItem("on_load_counter", n);
-        nums = n.toString().split('').map(Number);
-        document.getElementById('CounterVisitor').innerHTML = '';
-        for (var i of nums) {document.getElementById('CounterVisitor').innerHTML += '<span class="counter-item">' + i + '</span>';}
